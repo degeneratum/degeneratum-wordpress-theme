ce0304aae36124833c90b95c7cf0dd6c4909e36d
Ryan Stockdale <unitsix@me.com>
2017-12-17 20:33:50 +1100

    video h tag change to match

--- bootstrap-starter/video_posts_loop_template.php
+++ bootstrap-starter/video_posts_loop_template.php
@@ -1,14 +1,15 @@
 <!-- Post Wrap Start-->
 <div class="post hentry ivycat-post col-xs-12 col-md-6">
 	
-	<!-- 	This outputs the post TITLE -->
-	<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+	
 
 	<!-- 	This outputs the post.  To display experpt content including images and html, 
 		replace the_excerpt(); with the_content();  below. -->
 	<div class="entry-summary entry-summary-video">
 		<?php the_content(); ?>
 	</div>
+    <!-- 	This outputs the post TITLE -->
+	<h4 class="video-header entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 
 </div>
 <!-- // Post Wrap End -->
